4c8d8f9d66b505326090fc833678940ba7893af5
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-10-10 00:08:13 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts '-> CODES:'
+codes = [
+  {code: 'X99.7', description: 'Not known'},
+  {code: 'X99.8', description: 'Outpatient procedure carried out but no appropriate OPCS-4 code available'},
+  {code: 'X99.9', description: 'No outpatient procedure carried out'},
+]
+
+Code.create(codes).each do | code |
+  puts 'Code: ' << code[:code]
+end
