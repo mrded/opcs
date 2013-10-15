2dee2b333a40a05a4b913437516040d776683f51
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-10-16 00:27:34 +0100

    Add searching.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,6 +1,11 @@
 class CodesController < ApplicationController
   def index
-    @codes = Code.all
+    if params[:search]
+      @codes = Code.find(:all, conditions: ['code LIKE ?', "%#{params[:search]}%"])
+    else
+      @codes = Code.all
+    end
+
     render json: @codes if params[:format] == 'json'
   end
 
