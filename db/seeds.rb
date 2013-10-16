0a3ce01702e069a77b08d7ed8d6e9963d6dc50cc
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-10-16 23:50:12 +0100

    Import categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
+categories = []
+
+puts '-> CATEGORIES:'
+CSV.foreach Rails.root.join('db', 'import', 'categories.csv') do |row|
+  categories << {
+    chapter: row[0],
+    name: row[1],
+  }
+end
+
+Category.create(categories)
+puts 'DONE!'
+
+#####################
 puts '-> CODES:'
 codes = []
 
 CSV.foreach Rails.root.join('db', 'import', 'codes.csv') do |row|
   codes << {
-    code: row[0],
+    name: row[0],
     description: row[1],
+    category_id: Category.where(chapter: row[0][0]).first[:id]
   }
 end
 
-Code.create(codes).each do | code |
-  puts 'Code: ' << code[:code]
-end
+Code.create(codes)
+puts 'DONE!'
