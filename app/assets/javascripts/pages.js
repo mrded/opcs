16afb8fc86c73792d63bf93820003eefc4105d8f
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-05 01:23:47 +0000

    Simplify.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,19 +1,12 @@
 var opcsApp = angular.module('opcsApp', []);
 
-opcsApp.controller('FetchCtrl', function FetchCtrl($scope, $http, $templateCache) {
+opcsApp.controller('FetchCtrl', function FetchCtrl($scope, $http) {
     $scope.searchText = function() {
       // If enter has been pressed.
       if (event.keyCode == 13 && $scope.search){
-        $http({method: 'GET', url: 'codes.json?search=' + $scope.search, cache: $templateCache}).
-          success(function(data) {
-            $scope.codes = data;
-          }).
-          error(function(data, status) {
-            $scope.codes = [{
-              name: status,
-              description: "Request failed"
-            }];
-          });
+        $http.get('codes.json?search=' + $scope.search).then(function(result){
+          $scope.codes = result.data;
+        });
       }
     }
 });
