4f4e2b6172c408a729310ce477ab5bdf781ddc4d
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-05 01:26:37 +0000

    Delete spaces.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,12 +1,12 @@
 var opcsApp = angular.module('opcsApp', []);
 
 opcsApp.controller('FetchCtrl', function FetchCtrl($scope, $http) {
-    $scope.searchText = function() {
-      // If enter has been pressed.
-      if (event.keyCode == 13 && $scope.search){
-        $http.get('codes.json?search=' + $scope.search).then(function(result){
-          $scope.codes = result.data;
-        });
-      }
+  $scope.searchText = function() {
+    // If enter has been pressed.
+    if (event.keyCode == 13 && $scope.search){
+      $http.get('codes.json?search=' + $scope.search).then(function(result){
+        $scope.codes = result.data;
+      });
     }
+  }
 });
