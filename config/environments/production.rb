a7cd38b78e59ff9904d370c4935c1edc16036047
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-05 01:44:36 +0000

    Do not compress js and css.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,14 @@ Opcs::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  class NoCompression
+    def compress(string)
+      string # do nothing
+    end
+  end
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
