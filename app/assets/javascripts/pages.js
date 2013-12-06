fd3d5211b1c53626fef57a81d881920901c058f0
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-06 00:12:34 +0000

    Save page's results.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,12 +1,17 @@
 var opcsApp = angular.module('opcsApp', []);
 
-opcsApp.controller("FetchCtrl", ['$scope', '$http', function($scope, $http) {
-  $scope.searchText = function() {
-    // If enter has been pressed.
-    if (event.keyCode == 13 && $scope.search){
+opcsApp.controller("FetchCtrl", [
+  '$scope', '$http', '$location', function($scope, $http, $location) {
+
+    $scope.searchText = function() {
       $http.get('codes.json?search=' + $scope.search).then(function(result){
         $scope.codes = result.data;
+        $location.search({search: $scope.search}).replace();
       });
     }
-  }
-}]);
+
+    // Init.
+    $scope.search = $location.search().search
+    $scope.searchText();
+  }]
+);
