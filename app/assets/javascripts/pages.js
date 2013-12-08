9d73fb2d82a890fa40eb0537346ee842ad92f793
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-08 02:45:01 +0000

    Simplify.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,7 @@
 var opcsApp = angular.module('opcsApp', []);
 
 opcsApp.controller("FetchCtrl", [
-  '$scope', '$http', '$location', function($scope, $http, $location) {
+  '$scope', '$http', '$location', function($scope, $http) {
 
     var progressMode = function(status) {
       $scope.hideProgress = !status;
@@ -13,14 +13,11 @@ opcsApp.controller("FetchCtrl", [
 
       $http.get('codes.json?search=' + $scope.search).then(function(result) {
         progressMode(false);
-
         $scope.codes = result.data;
-        $location.search({search: $scope.search || ''}).replace();
       });
     }
 
     // Init.
-    $scope.search = $location.search().search
-    $scope.searchText();
+    progressMode(false);
   }]
 );
